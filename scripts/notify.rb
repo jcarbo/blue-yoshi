1db0ede0ea5d7b775aadd99f930100c4944ce9bc
Jeff Carbonella <jeff@contactually.com>
2016-06-28 21:40:42 -0400

    Update to send evening notifications

--- scripts/notify.rb
+++ scripts/notify.rb
@@ -1,5 +1,7 @@
 require_relative '../setup'
 
+Time.zone = 'Eastern Time (US & Canada)'
+
 GOOGLE_CLIENT = GoogleClient.new
 TWILIO_CLIENT = Twilio::REST::Client.new(
   ENV['TWILIO_ACCOUNT_SID'],
@@ -8,7 +10,7 @@ TWILIO_CLIENT = Twilio::REST::Client.new(
 
 def email_message(data)
   formatted_date = Date.parse(data[:end_date]).strftime('%A, %B %-d')
-  personalized_link = "#{ENV['QUESTIONNAIRE_LINK']}&CID=#{data[:cid]}"
+  personalized_link = "#{data[:link]}&CID=#{data[:cid]}"
 
   lines = [
     "Hi #{data[:first_name]}!",
@@ -26,7 +28,7 @@ end
 
 def sms_message(data)
   formatted_date = Date.parse(data[:end_date]).strftime('%A, %B %-d')
-  personalized_link = "#{ENV['QUESTIONNAIRE_LINK']}&CID=#{data[:cid]}"
+  personalized_link = "#{data[:link]}&CID=#{data[:cid]}"
 
   lines = [
     "Hi #{data[:first_name]}! Please take a moment to fill out your daily sleep survey: #{personalized_link}",
@@ -40,7 +42,7 @@ end
 def round_down_to_30_minutes(time)
   round_to_seconds = 30 * 60
 
-  Time.at(time.to_i / round_to_seconds * round_to_seconds).localtime('-05:00')
+  Time.at(time.to_i / round_to_seconds * round_to_seconds)
 end
 
 def hour_minute(time_or_string)
@@ -52,27 +54,64 @@ def hour_minute(time_or_string)
     .gsub(/^(\d{3})$/, '0\1') # 0-pad the hour if necessary
 end
 
-def send_email(email, columns)
+def send_email(email, attributes)
   return unless email && !email.empty?
 
   message = Mail.new do
-    to "#{columns[:first_name]} #{columns[:last_name]} <#{email}>".strip
+    to "#{attributes[:first_name]} #{attributes[:last_name]} <#{email}>".strip
     from "#{ENV['EMAIL_NAME']} <#{ENV['EMAIL_USERNAME']}>"
     subject 'Reminder - Complete your daily sleep survey'
-    body email_message(columns)
+    body email_message(attributes)
   end
 
   GOOGLE_CLIENT.send_message(message)
 end
 
-def send_sms(phone, columns)
+def send_sms(phone, attributes)
   return unless phone && !phone.empty?
 
   TWILIO_CLIENT.account.messages.create(
     :from => ENV['TWILIO_PHONE_NUMBER'],
     :to => phone,
-    :body => sms_message(columns)
+    :body => sms_message(attributes)
   )
+end
+
+def send_notification(type, attributes)
+  rounded_time = round_down_to_30_minutes(Time.now)
+
+  name = "#{attributes[:first_name]} #{attributes[:last_name]}"
+
+  if type == :morning && hour_minute(attributes[:morning_time]) != hour_minute(rounded_time)
+    puts "Skipping #{name} because their morning notification time is #{attributes[:morning_time]} and it's #{rounded_time}."
+    return
+  end
+
+  if type == :evening && hour_minute(attributes[:evening_time]) != hour_minute(rounded_time)
+    puts "Skipping #{name} because their evening notification time is #{attributes[:evening_time]} and it's #{rounded_time}."
+    return
+  end
+
+  link =
+    case type
+    when :morning then ENV['MORNING_QUESTIONNAIRE_LINK']
+    when :evening then ENV['EVENING_QUESTIONNAIRE_LINK']
+    else
+      return
+    end
+
+  messages_sent = []
+
+  if send_email(attributes[:email], attributes.merge(:link => link))
+    messages_sent << "email (#{attributes[:email]})"
+  end
+
+  if send_sms(attributes[:phone], attributes.merge(:link => link))
+    messages_sent << "SMS (#{attributes[:phone]})"
+  end
+
+  puts "++ Sent #{messages_sent.empty? ? 'nothing' : messages_sent.join(', ')} to #{name}"
+  sleep(1)
 end
 
 response = Faraday.get("https://docs.google.com/spreadsheets/d/#{ENV['GOOGLE_SHEET_ID']}/export?format=csv")
@@ -81,24 +120,22 @@ participants = CSV.parse(response.body)[1..-1]
 puts "Starting sync #{Time.now} for #{participants.count} participants"
 
 participants.each do |row|
-  first_name, last_name, email, phone, cid, notification_time, start_date, end_date = row
+  first_name, last_name, email, phone, cid, morning_time, evening_time, start_date, end_date = row
 
-  columns = {
+  attributes = {
     :first_name => first_name,
     :last_name => last_name,
     :email => email,
     :phone => phone,
     :cid => cid,
-    :notification_time => notification_time,
+    :morning_time => morning_time,
+    :evening_time => evening_time,
     :start_date => start_date,
     :end_date => end_date
   }
 
   name = "#{first_name} #{last_name}"
-  current_time = Time.now.localtime('-05:00')
-  current_date = current_time.to_date
-
-  rounded_time = round_down_to_30_minutes(current_time)
+  current_date = Time.now.to_date
 
   if current_date < Date.parse(start_date)
     puts "Skipping #{name} because it's before their start date."
@@ -110,21 +147,6 @@ participants.each do |row|
     next
   end
 
-  if hour_minute(notification_time) != hour_minute(rounded_time)
-    puts "Skipping #{name} because their notification time is #{notification_time} and it's #{rounded_time}."
-    next
-  end
-
-  messages_sent = []
-
-  if send_email(email, columns)
-    messages_sent << "email (#{email})"
-  end
-
-  if send_sms(phone, columns)
-    messages_sent << "SMS (#{phone})"
-  end
-
-  puts "++ Sent #{messages_sent.empty? ? 'nothing' : messages_sent.join(', ')} to #{name}"
-  sleep(1)
+  send_notification(:morning, attributes)
+  send_notification(:evening, attributes)
 end
